39f2f747cfe98b257fd9a960f6e96668c29ded4e
aakritimahaldar <123234867+aakritimahaldar@users.noreply.github.com>
2023-06-07 23:45:40 +0530

    Add files via upload

---
+++ FirstJavaProject/OverrideProg.java
@@ -0,0 +1,8 @@
+
+public class OverrideProg {
+	    public static void main(String[] args) {
+	        demo d1=new demo(); 
+	        d1.setenrollno("ABC968");
+	        System.out.println(d1);
+	    }
+}
---
+++ FirstJavaProject/demo.java
@@ -0,0 +1,27 @@
+
+public class demo {
+	    
+	    String Enrollno,result; 
+	    
+	    public String getenrollmenno()
+
+	    {
+	    
+	    return Enrollno;
+	    
+	    } 
+	    
+	    public void setenrollno(String enrollno)	
+	    
+	    { 
+	        this.Enrollno=enrollno;
+	    } 
+	    
+	    @Override
+	    
+	    public String toString() 
+	    { 
+	        return ("Enrollment number = "+ getenrollmenno());
+	    
+	    }
+	}
