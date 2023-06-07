54a59f3584e406a173f8371f0c27ee348ef7b84c
aakritimahaldar <123234867+aakritimahaldar@users.noreply.github.com>
2023-06-07 17:48:55 +0530

    Person.java

--- FirstJavaProject/Person.java
+++ FirstJavaProject/Person.java
@@ -1,20 +1,32 @@
 public class Person {
-    private String name;
-    private int age;
+	protected String name;
 
-    public Person(String name, int age) {
+    public Person(String name) {
         this.name = name;
-        this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
+    public void introduce() {
+        System.out.println("My name is " + name);
     }
 }
 
+  class Student extends Person {
+    private int grade;
 
+    public Student(String name, int grade) {
+        super(name);
+        this.grade = grade;
+    }
 
+    public void study() {
+        System.out.println(name + " is studying in grade " + grade);
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Student student = new Student("John", 10);
+        student.introduce();
+        student.study();
+    }
+}
